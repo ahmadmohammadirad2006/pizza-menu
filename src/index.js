e2da08ca6f823f4a8a4d53dbf010bf7c81c86690
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-09-07 11:28:52 +0330

    feat: passing props to pizza components
    customize pizza components

--- src/index.js
+++ src/index.js
@@ -69,11 +69,34 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='pizzas/spinaci.jpg'
+        price={10}
+      />
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomato, mozarella, mushrooms, and onion'
+        photoName='pizzas/funghi.jpg'
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -90,16 +113,6 @@ function Footer() {
     </footer>
   );
   // return React.createElement('footer', null, "We're currently open!");
-}
-
-function Pizza() {
-  return (
-    <>
-      <img src='pizzas/spinaci.jpg' alt='Pizza Spinaci'></img>
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </>
-  );
 }
 
 // React v18
